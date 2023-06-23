4c3ae027a014a1decee4279e96ee86c0fe10ead0
Anton Trafimovich <tonytrof@gmail.com>
2023-06-23 12:29:02 +0200

    Updated time.

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ import { down, left, mouse, right, up } from "@nut-tree/nut-js";
 const height = 200;
 const width = 200;
 
-mouse.config.mouseSpeed = 200;
-(async () => {
-  await mouse.move(down(height));
-  await mouse.move(right(width));
-  await mouse.move(up(height));
-  await mouse.move(left(width));
-})();
+mouse.config.mouseSpeed = 100;
+
+setInterval(async ()  => {
+  try {
+    await mouse.move(down(height));
+    await mouse.move(right(width));
+    await mouse.move(up(height));
+    await mouse.move(left(width));
+  } catch (err) {
+    console.log(err)
+  }
+}, 1000 * 60 * 2)
